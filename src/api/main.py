82d84ef86ad5bb114c5cb7d3b9ef5238389967e3
PabloRR100 <pabloruizruiz10@gmail.com>
2024-11-16 14:24:03 +0100

    Added first endpoints for collections and documents

--- src/api/main.py
+++ src/api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from api.routers import router
 
+
 app = FastAPI()
 app.include_router(router)
 
@@ -10,5 +11,4 @@ if __name__ == "__main__":
     
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
-
 
--- src/services/collections.py
+++ src/services/collections.py
@@ -1,21 +1,34 @@
 from abc import ABC, abstractmethod
 
+from schemas.collection import Collection, CollectionCreate
+
 
 class CollectionManager(ABC):
+    """
+    Abstract class for managing collections.
+    """
 
     @abstractmethod
-    def list_collections() -> list:
+    def list_collections() -> list[Collection]:
         ...
 
     @abstractmethod
-    def get_collection(collection_id: int) -> dict:
+    def get_collection(collection_id: int, *args, **kwargs) -> Collection:
         ...
 
     @abstractmethod
-    def create_collection(collection: dict) -> dict:
+    def get_collection_by_name(collection_name: str, *args, **kwargs) -> Collection:
         ...
 
     @abstractmethod
-    def delete_collection(collection_id: int) -> dict:
+    def create_collection(collection: CollectionCreate, *args, **kwargs) -> Collection:
+        ...
+
+    @abstractmethod
+    def update_collection(collection_id: int, collection: Collection) -> Collection:
+        ...
+
+    @abstractmethod
+    def delete_collection(collection_id: int) -> Collection:
         ...
     
